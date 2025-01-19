025eb618c065d8c1b80445a5bf4b58c84ab31a23
olekal <oleksandra.kalmykova@kindredgroup.com>
2025-01-19 18:58:08 +0100

    fix getting 403 on visit

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    chromeWebSecurity: false,
+    userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
     testIsolation: true,
     defaultCommandTimeout: 10000,
     baseUrl: "https://www.imdb.com",
